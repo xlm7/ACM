91c9dd82613135f0868d96dae56e0e3777fd1d3a
xlm7 <1182789409@qq.com>
2016-05-10 19:00:03 +0800

    添加ScrewMatrix代码

--- begin/ScrewMatrix/ScrewMatrix.java
+++ begin/ScrewMatrix/ScrewMatrix.java
@@ -2,21 +2,44 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		int n,i,s,x[],max,min,j;
+		int m,n,i,j,x1[][],p,q,a,b,c;
 		while(sc.hasNext()){
 			n=sc.nextInt();
-			x=new int[n+1];
-			for(i=1;i<=n;i++){
-			   for(j=i;j<=n;j+=i){
-				   x[j]=1-x[j];
-			   }
+			m=sc.nextInt();
+			x1=new int[n][m];
+			p=1;
+			c=Math.min(n, m);
+			q=(c+1)/2;			
+			for(i=0;i<q;i++){
+				//上
+				for(j=i;j<m-1-i;j++){
+					x1[i][j]=p++;
+				}						
+				//右
+				for(j=i;j<n-1-i;j++){
+					x1[j][m-i-1]=p++;
+				}				
+				//下
+				for(j=m-i-1;j>i;j--){
+					x1[n-1-i][j]=p++;
+				}				
+			//左
+				for(j=n-i-1;j>i;j--){
+					x1[j][i]=p++;
+				}
+	}	
+			//用来处理行数等于列数且为奇数的时候，最中间的那个数
+			if(m==n&&m%2==1){
+				x1[m/2][n/2]=p;
 			}
-			s=0;
-			for(i=1;i<=n;i++){
-				if(x[i]==1)
-					s++;					
-			}
-			System.out.println(s);
+			for(a=0;a<n;a++){
+			for(b=0;b<m;b++){
+				if(b!=m-1)
+				  System.out.printf("%2d ",x1[a][b]);
+				else
+					 System.out.printf("%2d\n",x1[a][b]);
+			}			
+		}
+}	
 	}
-}
-}
+	}
