b2cef4bbeba47babcb354fb70ba200dd6f9b5168
xlm7 <1182789409@qq.com>
2016-05-11 17:08:53 +0800

    添加DecimalConversion

--- begin/DecimalConversion/DecimalConversion.java
+++ begin/DecimalConversion/DecimalConversion.java
@@ -2,21 +2,39 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		int n,i,s,x[],max,min,j;
+		int m,n,p,q,flag;
+		char[]ch=new char[20];
+		
 		while(sc.hasNext()){
 			n=sc.nextInt();
-			x=new int[n+1];
-			for(i=1;i<=n;i++){
-			   for(j=i;j<=n;j+=i){
-				   x[j]=1-x[j];
-			   }
-			}
-			s=0;
-			for(i=1;i<=n;i++){
-				if(x[i]==1)
-					s++;					
-			}
-			System.out.println(s);
+			m=sc.nextInt();
+				ch[0]=0;
+				flag=1;
+				if(n<0){
+					flag=-1;
+					n=-1*n;
+				}
+				while(n!=0){					
+					ch[0]++;
+					p=n%m;
+                              n=n/m;
+					if(p>9){
+						ch[ch[0]]=(char) ('A'+p-10);
+					}else{
+						ch[ch[0]]=(char) ('0'+p);
+					}
+					
+				}
+				if(flag==-1)
+					{System.out.print("-");
+					
+					}
+				for(q=ch[0];q>=1;q--){
+					
+					System.out.print(ch[q]);
+				}
+				System.out.println();
+			
+}	
 	}
-}
-}
+	}
